b425b772c89ffbcc71073f6c717545ec4c4ae141
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-12-05 04:27:24 -0800

    Add IoC container processing

--- Hotel.DAL/Repositories/RoomCategoryRepository.cs
+++ Hotel.DAL/Repositories/RoomCategoryRepository.cs
@@ -8,55 +8,52 @@ using Hotel.DAL.Interfaces;
 
 namespace Hotel.DAL.Repositories
 {
-    namespace Hotel.DAL.Repositories
+    public class RoomCategoryRepository : IRoomCategoryRepository
     {
-        public class RoomCategoryRepository : IRoomCategoryRepository
+        private readonly HotelContext _context;
+
+        public RoomCategoryRepository(HotelContext context)
         {
-            private readonly HotelContext _context;
+            _context = context;
+        }
 
-            public RoomCategoryRepository(HotelContext context)
-            {
-                _context = context;
-            }
+        public IEnumerable<RoomCategory> GetAll()
+        {
+            return _context.RoomCategories;
+        }
 
-            public IEnumerable<RoomCategory> GetAll()
-            {
-                return _context.RoomCategories;
-            }
+        public RoomCategory Get(int id)
+        {
+            return _context.RoomCategories.Find(id);
+        }
 
-            public RoomCategory Get(int id)
-            {
-                return _context.RoomCategories.Find(id);
-            }
+        public IEnumerable<RoomCategory> Find(Func<RoomCategory, bool> predicate)
+        {
+            return _context.RoomCategories.Where(predicate).ToList();
+        }
 
-            public IEnumerable<RoomCategory> Find(Func<RoomCategory, bool> predicate)
-            {
-                return _context.RoomCategories.Where(predicate).ToList();
-            }
+        public void Create(RoomCategory item)
+        {
+            _context.RoomCategories.Add(item);
+        }
 
-            public void Create(RoomCategory item)
-            {
-                _context.RoomCategories.Add(item);
-            }
+        public void Update(RoomCategory item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+        }
 
-            public void Update(RoomCategory item)
+        public void Delete(int id)
+        {
+            var roomCategory = _context.RoomCategories.Find(id);
+            if (roomCategory != null)
             {
-                _context.Entry(item).State = EntityState.Modified;
+                _context.RoomCategories.Remove(roomCategory);
             }
+        }
 
-            public void Delete(int id)
-            {
-                var roomCategory = _context.RoomCategories.Find(id);
-                if (roomCategory != null)
-                {
-                    _context.RoomCategories.Remove(roomCategory);
-                }
-            }
-
-            public RoomCategory FindById(int id)
-            {
-                return _context.RoomCategories.Find(id);
-            }
+        public RoomCategory FindById(int id)
+        {
+            return _context.RoomCategories.Find(id);
         }
     }
 }
