dac1cc6ee24beed4990687e196a5b166943a754b
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-12-05 03:58:56 -0800

    Add BLL DTOs

---
+++ Hotel.BLL/DTO/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel.BLL.DTO
+{
+    public class UserDto
+    {
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; }
+    }
+}
